765e3ec0d1b04f6ad69e1e417bf98b4ee9e58d39
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2016-09-06 17:29:53 +0200

    add so builder and cdef
    add cffi based so builder for dev usage, setuptools integration will
    follow later, and cdef definition for building the low level binding

---
+++ ffi_build.py
@@ -0,0 +1,25 @@
+
+from cffi import FFI
+from os import path
+
+
+BASEDIR = path.dirname(path.realpath(__file__))
+
+
+ffibuilder = FFI()
+
+ffibuilder.set_source("_rtrlib",
+                      """
+                      #include <rtrlib/rtrlib.h>
+                      #include <sys/types.h>
+                      #include <pthread.h>
+                      """,
+                      libraries=['rtr'])
+
+
+with open(path.join(BASEDIR, "rtrlib.cdef")) as file_obj:
+    ffibuilder.cdef(file_obj.read())
+
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
