bcb749d30d2b23fa790eb34c58b8417fed50538d
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2016-11-11 09:46:45 +0100

    docs: remove python path injection

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,6 @@ from __future__ import unicode_literals
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
