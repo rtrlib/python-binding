6535412216a6d8c95024bd29cef18cfbc6968ed3
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2017-02-14 16:26:26 +0100

    Support build against librtr without ssh support
    this commit also adds a way to detecti from python whether ssh support
    is available or not.
    
    Fixes #7

--- ffi_build.py
+++ ffi_build.py
@@ -8,13 +8,6 @@ BASEDIR = path.dirname(path.realpath(__file__))
 
 ffibuilder = FFI()
 
-ffibuilder.set_source("_rtrlib",
-                      """
-                      #include <rtrlib/rtrlib.h>
-                      """,
-                      libraries=['rtr'])
-
-
 with open(path.join(BASEDIR, "rtrlib.cdef")) as file_obj:
     ffibuilder.cdef(file_obj.read())
 
@@ -23,6 +16,36 @@ ffibuilder.cdef("""
         extern "Python" void pfx_update_callback(struct pfx_table *pfx_table, const struct pfx_record record, const bool added);
         extern "Python" void spki_update_callback(struct spki_table *spki_table, const struct spki_record record, const bool added);
                 """)
+
+ffibuilder.set_source("_rtrlib",
+                      """
+                      #include <rtrlib/rtrlib.h>
+
+                      /*
+                       * check if ssh support is available
+                       * if not define tr_ssh_config struct
+                       * and have_ssh with return 0
+                       * otherwise just define have_ssh with return 1
+                       */
+                      #ifndef RTRLIB_HAVE_LIBSSH
+                      struct tr_ssh_config {
+                          char *host;
+                          unsigned int port;
+                          char *bindaddr;
+                          char *username;
+                          char *server_hostkey_path;
+                          char *client_privkey_path;
+                      };
+                      int have_ssh(void) {return 0;}
+                      #endif
+
+                      #ifdef RTRLIB_HAVE_LIBSSH
+                      int have_ssh(void) {return 1;}
+                      #endif
+                      """,
+                      libraries=['rtr'])
+
+
 
 if __name__ == "__main__":
     ffibuilder.compile(verbose=True)
